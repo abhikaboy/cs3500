b2cf921a8f951b68a20807db4294bebcae772a98
Abhik Ray <abhikaboy@gmail.com>
2024-06-11 19:03:17 -0400

    renamed shares

--- Stocks/src/src/model/Portfolio.java
+++ Stocks/src/src/model/Portfolio.java
@@ -9,13 +9,13 @@ import java.util.HashMap;
 public class Portfolio {
 
   // Symbol, Share
-  HashMap<String, Share> stocks;
+  HashMap<String, Share> shares;
 
   /**
    * Constructor for a new portfolio.
    */
   public Portfolio() {
-    stocks = new HashMap<>();
+    shares = new HashMap<>();
   }
 
   /**
@@ -24,12 +24,12 @@ public class Portfolio {
    * @param portfolio Portfolio to copy.
    */
   public Portfolio(Portfolio portfolio) {
-    stocks = new HashMap<>();
-    for (String symbol : portfolio.stocks.keySet()) {
-      stocks.put(symbol,
+    shares = new HashMap<>();
+    for (String symbol : portfolio.shares.keySet()) {
+      shares.put(symbol,
               new Share(symbol,
-                      portfolio.stocks.get(symbol).getQuantity(),
-                      portfolio.stocks.get(symbol).getData()));
+                      portfolio.shares.get(symbol).getQuantity(),
+                      portfolio.shares.get(symbol).getData()));
     }
   }
 
@@ -41,8 +41,8 @@ public class Portfolio {
    * @return The quantity of the stock in the portfolio.
    */
   public int getStockQuantity(String symbol) {
-    if (stocks.containsKey(symbol)) {
-      return stocks.get(symbol).getQuantity();
+    if (shares.containsKey(symbol)) {
+      return shares.get(symbol).getQuantity();
     } else {
       throw new IllegalArgumentException("Stock not found in portfolio");
     }
@@ -56,27 +56,19 @@ public class Portfolio {
    * @param quantity Quantity of the stock to buy.
    */
   public void buyStock(String symbol, HashMap<String, StockRow> stock, int quantity) {
-    System.out.println("Buying " + quantity + " shares of " + symbol);
-    System.out.println(this.portfolioAString());
-    if (stocks.containsKey(symbol)) {
-      System.out.println("Stock already in portfolio");
-      stocks.get(symbol).purchase(quantity);
+    if (shares.containsKey(symbol)) {
+      shares.get(symbol).purchase(quantity);
     } else {
-      System.out.println("Adding stock to portfolio");
-      stocks.put(symbol, new Share(symbol, quantity, stock));
+      shares.put(symbol, new Share(symbol, quantity, stock));
     }
   }
 
   public void buyStock(String symbol, HashMap<String, StockRow> stock, int quantity, String date) {
-    System.out.println("Buying " + quantity + " shares of " + symbol);
-    System.out.println(this.portfolioAString());
-    if (stocks.containsKey(symbol)) {
-      System.out.println("Stock already in portfolio");
-      stocks.get(symbol).purchase(quantity, date);
+    if (shares.containsKey(symbol)) {
+      shares.get(symbol).purchase(quantity, date);
     } else {
-      System.out.println("Adding stock to portfolio");
-      stocks.put(symbol, new Share(symbol, quantity, stock, date));
-    }
+      shares.put(symbol, new Share(symbol, quantity, stock, date));
+    } 
   }
 
   /**
@@ -86,10 +78,10 @@ public class Portfolio {
    * @param quantity Quantity of the stock to sell.
    */
   public void sellStock(String symbol, int quantity) {
-    if (stocks.containsKey(symbol)) {
-      stocks.get(symbol).sell(quantity);
-      if (stocks.get(symbol).getQuantity() == 0) {
-        stocks.remove(symbol);
+    if (shares.containsKey(symbol)) {
+      shares.get(symbol).sell(quantity);
+      if (shares.get(symbol).getQuantity() == 0) {
+        shares.remove(symbol);
       }
     } else {
       throw new IllegalArgumentException("Stock not found in portfolio");
@@ -104,13 +96,13 @@ public class Portfolio {
 
   public double getPortfolioValue() {
     double total = 0;
-    for (String symbol : stocks.keySet()) {
-      HashMap<String, StockRow> stock = stocks.get(symbol).getData();
+    for (String symbol : shares.keySet()) {
+      HashMap<String, StockRow> stock = shares.get(symbol).getData();
       StockRow lastRow = null;
       // todays date: 
       String date = "2024-06-06";
       lastRow = stock.get(date);
-      total += lastRow.getClose() * stocks.get(symbol).getQuantity();
+      total += lastRow.getClose() * shares.get(symbol).getQuantity();
     }
     return total;
   }
@@ -123,32 +115,32 @@ public class Portfolio {
    */
   public double getPortfolioValue(String date) {
     double total = 0;
-    for (String symbol : stocks.keySet()) {
-      HashMap<String, StockRow> stock = stocks.get(symbol).getData();
+    for (String symbol : shares.keySet()) {
+      HashMap<String, StockRow> stock = shares.get(symbol).getData();
       StockRow lastRow = stock.get(date);
       // todays date: 
-      total += lastRow.getClose() * stocks.get(symbol).getQuantity();
+      total += lastRow.getClose() * shares.get(symbol).getQuantity();
     }
     return total;
   }
 
   /**
    * Get the size of this portfolio by counting the size of the HashMap. Essentially, returns
-   * the total amount of stocks present.
+   * the total amount of shares present.
    *
-   * @return int. The total amount of stocks in this portfolio.
+   * @return int. The total amount of shares in this portfolio.
    */
   public int getPortfolioSize() {
-    return stocks.size();
+    return shares.size();
   }
 
   /**
-   * Get the names of all the stocks.
+   * Get the names of all the shares.
    *
-   * @return the names of all the stocks
+   * @return the names of all the shares
    */
   public String[] getStockNames() {
-    return stocks.keySet().toArray(new String[0]);
+    return shares.keySet().toArray(new String[0]);
   }
 
   /**
@@ -159,8 +151,8 @@ public class Portfolio {
    */
   public String portfolioAString() {
     String output = "";
-    for (String symbol : stocks.keySet()) {
-      output += symbol + " " + stocks.get(symbol).getQuantity() + "\n";
+    for (String symbol : shares.keySet()) {
+      output += symbol + " " + shares.get(symbol).getQuantity() + "\n";
     }
     return output;
   }
@@ -172,7 +164,7 @@ public class Portfolio {
    * @return Stock with the corresponding inputted symbol.
    */
   public Share getShare(String symbol) {
-    return stocks.get(symbol);
+    return shares.get(symbol);
   }
 
 }
